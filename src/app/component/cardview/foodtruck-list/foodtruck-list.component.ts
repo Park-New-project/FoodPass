496f161f106fbc7ed8de10456ce3fd59e08c5c6e
mjkim <mjkim103301@gmail.com>
2020-05-22 14:04:15 +0900

    일단 에러

--- src/app/component/cardview/foodtruck-list/foodtruck-list.component.ts
+++ src/app/component/cardview/foodtruck-list/foodtruck-list.component.ts
@@ -8,16 +8,16 @@ import { FoodtruckData } from 'src/app/data/foodtruck';
 })
 export class CardviewComponent implements OnInit {
   
-  // @Input() name:string;
-  // @Input() locate:string;
-  // @Input() inform:string;
-  // @Input() wating: number;
-  // @Input() grade:number;
-  // @Input() notice:string;
-  // @Input() distance:number;
-  // @Input() truckImage:string;
+  @Input() name:string;
+  @Input() locate:string;
+  @Input() inform:string;
+  @Input() wating: number;
+  @Input() grade:number;
+  @Input() notice:string;
+  @Input() distance:number;
+  @Input() truckImage:string;
 
-  @Input() truckIndex : number;
+//  @Input() truckIndex : number;
 
 
   constructor(
@@ -28,10 +28,10 @@ export class CardviewComponent implements OnInit {
     
     
   }
-  get truck(): TruckOrder{
-    return this.truckCtrl.items[this.truckIndex] as TruckOrder;
-  }
-  get foodtruck(){
-    return this.
-  }
+  // get truck(): TruckOrder{
+  //   return this.truckCtrl.items[this.truckIndex] as TruckOrder;
+  // }
+  // get foodtruck(){
+  //   return this.
+  // }
 }
