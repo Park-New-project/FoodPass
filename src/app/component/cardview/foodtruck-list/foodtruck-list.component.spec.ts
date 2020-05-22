f9ea1ad0064dc4bb78bbf1e01af5b2faa86fbe87
mjkim <mjkim103301@gmail.com>
2020-05-22 14:05:17 +0900

    Revert "file rename"
    This reverts commit d77cf74a2315f10ae05e73db4f6e9c8458521e48.

--- src/app/component/cardview/foodtruck-list/foodtruck-list.component.spec.ts
+++ src/app/component/cardview/foodtruck-list/foodtruck-list.component.spec.ts
@@ -1,7 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
-import { CardviewComponent } from './foodtruck-list.component';
+import { CardviewComponent } from './cardview.component';
 
 describe('CardviewComponent', () => {
   let component: CardviewComponent;
